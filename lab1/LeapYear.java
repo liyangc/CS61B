c89d5e27d5f3fcab270af774553aeaf7c1b863c6
liyangc <liyangc@berkeley.edu>
2019-03-06 01:22:08 -0800

    completed lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,16 @@ public class LeapYear {
             }
         }
     }
-}
 
+    public static boolean isLeapYear(int year) {
+      if (year % 400 == 0){
+        return true;
+      } else {
+        if (year % 4 == 0 & year % 100 != 0){
+          return true;
+        } else {
+          return false;
+        }
+        }
+      }
+}
